e5fa8a6473e3e355282c1c2c587387df3074fc6b
max <max.house.web.development@gmail.com>
2023-06-27 04:56:08 +0300

    add PhoneLink to HeaderBottom

--- src/components/modules/Header/Bottom/Bottom.tsx
+++ src/components/modules/Header/Bottom/Bottom.tsx
@@ -1,13 +1,20 @@
 import { Container } from "@components/containers/Container";
+import { Phone as PhoneLink } from "@components/shared/ui/links/Phone";
 import { MobileDrawer } from "../MobileDrawer";
-import { useAppMediaQuery } from "@src/features/hooks/useAppMediaQuery";
+import { useAppMediaQuery } from "@features/hooks/useAppMediaQuery";
+import { HStack } from "@chakra-ui/react";
 
 export function Bottom() {
   const device = useAppMediaQuery();
 
   return (
     <Container minH={"45px"} mb={5} bgColor={"brand.gray"}>
-      {device === "isMobile" && <MobileDrawer />}
+      {device === "isMobile" && (
+        <HStack justifyContent={"space-between"}>
+          <MobileDrawer />
+          <PhoneLink />
+        </HStack>
+      )}
     </Container>
   );
 }
