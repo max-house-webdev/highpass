bb127c7c82fcbcde11556176536b640da74df9c3
max house <max.msp83@yandex.ru>
2023-12-11 21:32:36 +0300

    docs: add mh-hook docs

--- src/components/ui/buttons/LearnMore/LearnMore.tsx
+++ src/components/ui/buttons/LearnMore/LearnMore.tsx
@@ -1,7 +1,5 @@
 import { ButtonProps } from "@chakra-ui/react";
 
-import { useEvent } from "@mh-utils/hooks/useEvent";
-
 import { Primary } from "../Primary";
 
 export interface ILearnMoreProps extends ButtonProps {
@@ -11,7 +9,7 @@ export interface ILearnMoreProps extends ButtonProps {
 export function LearnMore(props: ILearnMoreProps) {
   const { textContent, ...rest } = props;
 
-  const onClick = useEvent(() => console.log("LearnMoreButton click"));
+  const onClick = () => console.log("LearnMoreButton click");
 
   return (
     <Primary
