8b03c0430556074361e1456b22ba6a79c4f412e0
max house <max.msp83@yandex.ru>
2023-12-13 05:46:25 +0300

    refactor: :fire: unesed lines

--- src/components/simple/BannerHeading/BannerHeading.tsx
+++ src/components/simple/BannerHeading/BannerHeading.tsx
@@ -1,17 +1,7 @@
 import { Heading, HeadingProps } from "@chakra-ui/react";
 
-export interface IHeadingProps extends HeadingProps {
-  // textContent: string;
-}
+export interface IHeadingProps extends HeadingProps {}
 
 export function BannerHeading(props: IHeadingProps) {
-  // const { textContent, ...rest } = props;
-
-  // return (
-  //   <Heading variant={"bannerHeading"} {...rest}>
-  //     {textContent}
-  //   </Heading>
-  // );
-
   return <Heading variant={"bannerHeading"} {...props} />;
 }
